78a50454c6ed4c8ff9d7a742f1436101ea0e2365
Yan Kun Meng <yankun.meng@stonybrook.edu>
2023-01-19 20:02:58 -0500

    basic jsonData

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,58 @@
-package org.example;public class Main {
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Random;
+import java.util.TimeZone;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ObjectMapper mapper = new ObjectMapper();
+
+//        // simulate data
+        long timeStamp = 1_499_070_300L;
+        LocalDateTime entime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timeStamp), TimeZone.getDefault().toZoneId());
+        timeStamp = 1_499_080_300L;
+        LocalDateTime extime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timeStamp), TimeZone.getDefault().toZoneId());
+//
+////        System.out.println(rd.nextLong());
+//
+//        // generate random epoch time upper bounded by time right now
+//        LocalDateTime ti = LocalDateTime.now();
+//
+//        ZonedDateTime zdt = ZonedDateTime.of(ti, ZoneId.systemDefault());
+//        long timeRightNow = zdt.toInstant().toEpochMilli();
+
+        try {
+            VisitRecord v1 = new VisitRecord("YangYang", entime, extime);
+            String jsonData = mapper.writeValueAsString(v1);
+            System.out.println(jsonData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//
+//        for (int i = 0; i < 10000; i++) {
+//            Random rd = new Random();
+//            String n = "YangYang";
+//            LocalDateTime entryTime = getRandomLocalDateTime(timeRightNow, rd);
+//            LocalDateTime exitTime = getRandomLocalDateTime(timeRightNow, rd);
+//            try {
+//                VisitRecord v = new VisitRecord(n, entryTime, exitTime);
+//                System.out.println(v);
+//            } catch (Exception ignored) {
+//
+//            }
+//        }
+    }
+
+    private static LocalDateTime getRandomLocalDateTime(long timeRightNow, Random rd) {
+        long timeStamp = Math.abs(rd.nextLong() % timeRightNow);
+        LocalDateTime time = LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), TimeZone.getDefault().toZoneId());
+        return time;
     }
 }
--- src/main/java/org/example/VisitRecord.java
+++ src/main/java/org/example/VisitRecord.java
@@ -1,2 +1,29 @@
-package org.example;public class VisitRecord {
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class VisitRecord {
+    // Attributes
+    String name;
+    LocalDateTime entry;
+    LocalDateTime exit;
+
+    // Constructors
+    public VisitRecord(String n, LocalDateTime en, LocalDateTime ex) throws Exception {
+        if(ex.isBefore(en)) throw new InvalidEntryExitTimeException();
+        this.name = n;
+        this.entry = en;
+        this.exit = ex;
+    }
+    public VisitRecord(String n, LocalDateTime en) throws Exception {
+        this.name = n;
+        this.entry = en;
+        this.exit = null;
+    }
+}
+
+class InvalidEntryExitTimeException extends Exception {
+    public InvalidEntryExitTimeException(){
+        super();
+    }
 }
